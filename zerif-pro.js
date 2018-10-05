059c1ed073201a1e10c7b45fc8e0c19f2c5fe9a1
cristian-ungureanu <cristian@themeisle.com>
2018-10-05 18:44:54 +0300

    zerif pre script

--- zerif-pro.js
+++ zerif-pro.js
@@ -1,1 +1,6 @@
 jQuery('#carousel-homepage-latestnews').carousel('pause');
+
+var windowWidth = jQuery(window).width();
+if ( windowWidth <= 460 ) {
+    jQuery('#carousel-homepage-latestnews').css( 'height', '1672px');
+}
